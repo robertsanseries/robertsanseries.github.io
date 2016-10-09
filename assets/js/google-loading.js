c2285f9674bd092a6f677151e069b0376cfb3ad0
Robert San <robertsanseries@gmail.com>
2016-10-09 03:08:46 -0300

    website version 0.1

---
+++ assets/js/google-loading.js
@@ -0,0 +1,20 @@
+function id(el) {
+	return document.getElementById(el);
+}
+
+function hide(el) {
+	id(el).style.display = 'none';
+}
+
+window.onload = function() {
+	time();
+}
+
+function time() {
+	setTimeout(function(){ finaly(); }, 3000);
+}
+	
+function finaly(){
+	id('body').style.display = 'block';
+	hide('loading');
+}
