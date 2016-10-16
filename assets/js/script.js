26cc4fb090be0e9929c256b3c2d365b7d5829ceb
robertsanseries <robertsanseries@gmail.com>
2016-10-16 00:15:30 -0300

    website version 0.3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,28 @@
-	function setaActionOver1(){
-		document.getElementById("seta1").style.marginLeft = "5px";
-	}
+function setaActionOver1(){
+	document.getElementById("seta1").style.marginLeft = "5px";
+}
 
-	function setaActionOut1(){
-		document.getElementById("seta1").style.marginLeft = "0px";
-	}
-	function setaActionOver2(){
-		document.getElementById("seta2").style.marginLeft = "5px";
-	}
+function setaActionOut1(){
+	document.getElementById("seta1").style.marginLeft = "0px";
+}
+function setaActionOver2(){
+	document.getElementById("seta2").style.marginLeft = "5px";
+}
 
-	function setaActionOut2(){
-		document.getElementById("seta2").style.marginLeft = "0px";
-	}
-	function setaActionOver3(){
-		document.getElementById("seta3").style.marginLeft = "5px";
-	}
+function setaActionOut2(){
+	document.getElementById("seta2").style.marginLeft = "0px";
+}
+function setaActionOver3(){
+	document.getElementById("seta3").style.marginLeft = "5px";
+}
 
-	function setaActionOut3(){
-		document.getElementById("seta3").style.marginLeft = "0px";
-	}
-	function setaActionOver4(){
-		document.getElementById("seta4").style.marginLeft = "5px";
-	}
+function setaActionOut3(){
+	document.getElementById("seta3").style.marginLeft = "0px";
+}
+function setaActionOver4(){
+	document.getElementById("seta4").style.marginLeft = "5px";
+}
 
-	function setaActionOut4(){
-		document.getElementById("seta4").style.marginLeft = "0px";
-	}
+function setaActionOut4(){
+	document.getElementById("seta4").style.marginLeft = "0px";
+}
